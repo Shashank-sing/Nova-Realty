ad655ba13e3cfd1682d299b679d70c18de80dd70
shashank <shashanksinghrocks99@gmail.com>
2021-02-14 12:05:38 +0530

    Uploading to heroku

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const path = require('path');
+
 
 const app = express();
+
 
 app.get('/api/homes', (req, res) => {
     const homes = [
@@ -16,6 +19,15 @@ app.get('/api/homes', (req, res) => {
 
     res.json(homes);
 });
+
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
+}
 
 const PORT = process.env.PORT || 5000;
 
